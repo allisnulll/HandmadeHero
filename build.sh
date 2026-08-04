3c8b61e11f7edd295bb1824a44379846f10feadb
allisnulll <133504785+allisnulll@users.noreply.github.com>
2026-08-04 17:57:52 -0400

    day 9: sine wave sound autput

--- build.sh
+++ build.sh
@@ -1,3 +1,56 @@
 #!/usr/bin/env bash
+
+prod=$1
+
+set -euo pipefail
 mkdir -p build
-clang code/main.c -o build/handmade -std=c11 -lSDL3 -ggdb -Wall -Werror
+
+cflags=(
+    -std=c11 -lSDL3 -lm -ggdb -O2
+    \
+    -Wall -Wextra -Wpedantic
+    -Werror=format-security -Werror=implicit
+    -Werror=incompatible-pointer-types -Werror=int-conversion
+    -Wconversion -Wsign-conversion -Wshadow -Wundef -Wimplicit-fallthrough
+    -Wstrict-prototypes -Wmissing-prototypes -Wold-style-definition
+    -Wformat -Wformat=2 -Wformat-overflow=2 -Wformat-truncation=2
+    -Wnull-dereference -Wnonnull-compare
+    -Wuninitialized -Winit-self -Wmaybe-uninitialized
+    -Wfloat-equal -Wwrite-strings
+    -Wjump-misses-init -Wlogical-op -Waggregate-return
+    -Wstack-protector -Wtrampolines -Wbidi-chars=any
+    \
+    -fno-delete-null-pointer-checks
+    -fno-strict-overflow
+    -fno-strict-aliasing
+    -fstrict-flex-arrays=3
+    -fstack-clash-protection -fstack-protector-strong
+    -fzero-init-padding-bits=all
+    -fcf-protection=full
+    -fPIE -pie
+    -fanalyzer
+    \
+    -U_FORTIFY_SOURCE
+    \
+    "-Wl,-z,nodlopen" "-Wl,-z,noexecstack"
+    "-Wl,-z,relro" "-Wl,-z,now"
+    "-Wl,--as-needed" "-Wl,--no-copy-dt-needed-entries"
+    \
+    src/main.c
+)
+
+if [ "$prod" ]; then
+    printf "\e[38;5;52mPRODUCTION\n\e[0m"
+    cflags+=(
+        -D_FORTIFY_SOURCE=3
+        -ftrivial-auto-var-init=zero
+    )
+else
+    cflags+=(
+        -Werror
+        "-fsanitize=address,undefined"
+        -fno-omit-frame-pointer
+    )
+fi
+
+gcc -o build/handmade "${cflags[@]}"
--- src/main.c
+++ src/main.c
@@ -1,52 +1,69 @@
 #include <SDL3/SDL.h>
-#include <SDL3/SDL_init.h>
-#include <SDL3/SDL_main.h>
+#include <math.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdint.h>
 
-#define FPS 60
 #define WINDOW_WIDTH 1280
 #define WINDOW_HEIGHT 720
 #define BYTES_PER_PIXEL 4
 #define AUDIO_SAMPLE_RATE 48000
 #define GAMEPAD_STICK_THRESHOLD 8000
 
-static bool is_running;
+#define PI 3.14169f
+bool is_running;
 
-static SDL_Window *window;
-static SDL_Renderer *renderer;
-static SDL_Gamepad *gamepad;
-static SDL_AudioStream *audio_stream;
+SDL_Window *window;
+SDL_Renderer *renderer;
+SDL_Gamepad *gamepad;
+SDL_AudioStream *audio_stream;
 
-static int x_offset, y_offset;
+typedef struct {
+    SDL_Texture *texture;
+    uint32_t *memory;
+    uint16_t width;
+    uint16_t height;
+    int pitch;
+} BackBuffer;
+
+typedef struct {
+    void *memory;
+    int32_t running_sample;
+    int16_t volume;
+    size_t len;
+    size_t samples;
+    int hz;
+    float period;
+} AudioBuffer;
+
+uint8_t x_offset, y_offset;
+int gaxis_left_x, gaxis_left_y;
 
 typedef struct {
     bool is_down;
     bool was_down;
 } Button;
-static Button up, down, left, right;
+Button up, down, left, right;
 
-static int gaxis_left_x, gaxis_left_y;
+void render_back_buffer(BackBuffer *back_buffer);
+void resize_back_buffer(BackBuffer *back_buffer);
+void fill_audio_buffer(AudioBuffer *audio_buffer);
+void process_gamepad_button(SDL_GamepadButtonEvent gbutton);
+void process_key(SDL_KeyboardEvent key);
+void process_event(SDL_Event event, BackBuffer *back_buffer);
+int game_init(BackBuffer *back_buffer, AudioBuffer *audio_buffer);
+void game_tick(void);
 
-typedef struct {
-    SDL_Texture *texture;
-    uint32_t *memory;
-    int width;
-    int height;
-    int pitch;
-} BackBuffer;
-
-static void render_back_buffer(BackBuffer *back_buffer) {
+void render_back_buffer(BackBuffer *back_buffer) {
     uint8_t *row = (uint8_t*)back_buffer->memory;
     for (int y = 0; y < back_buffer->height; ++y) {
         uint32_t *pixel = (uint32_t*)row;
         for (int x = 0; x < back_buffer->width; ++x) {
-            uint8_t red = x + x_offset;
-            uint8_t green = y + y_offset;
+            uint8_t red = (uint8_t)x + x_offset;
+            uint8_t green = (uint8_t)y + y_offset;
             uint8_t blue = 0;
 
-            *pixel++ = (blue << 16) | (green << 8) | red;
+            *pixel++ = (uint32_t)(blue << 16) | (uint32_t)(green << 8) | (uint32_t)red;
         }
         row += back_buffer->pitch;
     }
@@ -59,8 +76,8 @@ static void render_back_buffer(BackBuffer *back_buffer) {
     );
 }
 
-static void resize_back_buffer(BackBuffer *back_buffer) {
-    SDL_GetWindowSize(window, &back_buffer->width, &back_buffer->height);
+void resize_back_buffer(BackBuffer *back_buffer) {
+    SDL_GetWindowSize(window, (int*)&back_buffer->width, (int*)&back_buffer->height);
     back_buffer->pitch = back_buffer->width * BYTES_PER_PIXEL;
 
     uint32_t *new_memory = realloc(
@@ -76,7 +93,44 @@ static void resize_back_buffer(BackBuffer *back_buffer) {
     render_back_buffer(back_buffer);
 }
 
-static void process_gamepad_button(SDL_GamepadButtonEvent gbutton) {
+void fill_audio_buffer(AudioBuffer *audio_buffer) {
+    if (!audio_stream) return;
+
+    audio_buffer->len = (AUDIO_SAMPLE_RATE * sizeof(int16_t)*2);
+    int queued = SDL_GetAudioStreamQueued(audio_stream);
+    if (queued > 0) {
+        audio_buffer->len -= (uint32_t)queued;
+        if (audio_buffer->len <= 0) return;
+    }
+    audio_buffer->samples = audio_buffer->len / sizeof(int16_t) / 2;
+    if (audio_buffer->running_sample >= AUDIO_SAMPLE_RATE)
+        audio_buffer->running_sample = 0;
+
+    int32_t starting_sample = audio_buffer->running_sample;
+    size_t len1 = 0;
+    for (size_t i = 0; i < audio_buffer->samples; ++i) {
+        if (audio_buffer->running_sample >= AUDIO_SAMPLE_RATE) {
+            audio_buffer->running_sample = 0;
+            len1 = i * sizeof(int16_t) * 2;
+        }
+
+        int16_t sound = (int16_t)(sin(2.0f*PI * (float)audio_buffer->running_sample / audio_buffer->period) * (float)audio_buffer->volume);
+
+        ((int16_t*)audio_buffer->memory)[audio_buffer->running_sample*2] = sound;
+        ((int16_t*)audio_buffer->memory)[audio_buffer->running_sample*2+1] = sound;
+
+        ++audio_buffer->running_sample;
+    }
+
+    if (len1) {
+        printf("HELLO");
+        SDL_PutAudioStreamData(audio_stream, (int32_t*)audio_buffer->memory + starting_sample, (int)len1);
+        SDL_PutAudioStreamData(audio_stream, (int32_t*)audio_buffer->memory, (int)(audio_buffer->len - len1));
+    } else
+        SDL_PutAudioStreamData(audio_stream, (int32_t*)audio_buffer->memory + starting_sample, (int)audio_buffer->len);
+}
+
+void process_gamepad_button(SDL_GamepadButtonEvent gbutton) {
     if (gbutton.which != SDL_GetGamepadID(gamepad)) return;
 
     switch(gbutton.button) {
@@ -101,7 +155,7 @@ static void process_gamepad_button(SDL_GamepadButtonEvent gbutton) {
     }
 }
 
-static void process_key(SDL_KeyboardEvent key) {
+void process_key(SDL_KeyboardEvent key) {
     switch (key.key) {
     case SDLK_ESCAPE:
     case SDLK_Q:
@@ -138,7 +192,7 @@ static void process_key(SDL_KeyboardEvent key) {
     }
 }
 
-static void process_event(SDL_Event event, BackBuffer *back_buffer) {
+void process_event(SDL_Event event, BackBuffer *back_buffer) {
     switch (event.type) {
 
     case SDL_EVENT_QUIT:
@@ -204,7 +258,7 @@ static void process_event(SDL_Event event, BackBuffer *back_buffer) {
     }
 }
 
-static int game_init(BackBuffer *back_buffer) {
+int game_init(BackBuffer *back_buffer, AudioBuffer *audio_buffer) {
     if (!SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_GAMEPAD | SDL_INIT_HAPTIC)) {
         printf("Couldn't initialize SDL: %s\n", SDL_GetError());
         return 1;
@@ -222,7 +276,7 @@ static int game_init(BackBuffer *back_buffer) {
         goto end1;
     }
 
-    SDL_GetWindowSize(window, &back_buffer->width, &back_buffer->height);
+    SDL_GetWindowSize(window, (int*)&back_buffer->width, (int*)&back_buffer->height);
     back_buffer->pitch = back_buffer->width * BYTES_PER_PIXEL;
     if (!(back_buffer->texture = SDL_CreateTexture(
         renderer,
@@ -235,9 +289,18 @@ static int game_init(BackBuffer *back_buffer) {
         goto end2;
     }
 
-    back_buffer->memory = malloc(back_buffer->width*back_buffer->height * BYTES_PER_PIXEL);
+    back_buffer->memory = malloc(back_buffer->width*back_buffer->height * (uint32_t)BYTES_PER_PIXEL);
     if (!back_buffer->memory) {
         printf("Couldn't allocate memory for back buffer.\n");
+        goto end3;
+    }
+
+    audio_buffer->len = (AUDIO_SAMPLE_RATE * sizeof(int16_t)*2);
+    audio_buffer->memory = malloc(audio_buffer->len);
+    if (!audio_buffer->memory) {
+        printf("Couldn't allocate memory for audio buffer.\n");
+
+        end3:
         SDL_DestroyTexture(back_buffer->texture);
         end2:
         SDL_DestroyWindow(window);
@@ -246,11 +309,16 @@ static int game_init(BackBuffer *back_buffer) {
         SDL_Quit();
         return 1;
     }
+    audio_buffer->running_sample = 0;
+    audio_buffer->volume = 2000;
+    audio_buffer->samples = AUDIO_SAMPLE_RATE;
+    audio_buffer->hz = 256;
+    audio_buffer->period = (float)AUDIO_SAMPLE_RATE / (float)audio_buffer->hz;
 
     return 0;
 }
 
-static void game_tick() {
+void game_tick(void) {
     if (right.is_down) ++x_offset;
     if (left.is_down) --x_offset;
     if (down.is_down) ++y_offset;
@@ -264,33 +332,10 @@ static void game_tick() {
     if (gaxis_left_y < -GAMEPAD_STICK_THRESHOLD && !up.is_down) --y_offset;
 }
 
-void audio() {
-    if (!audio_stream) return;
-
-    const int16_t tone_volume = 2000;
-    const int tone_hz = 256;
-    const int square_wave_period = AUDIO_SAMPLE_RATE / tone_hz;
-
-    int buffer_len = (AUDIO_SAMPLE_RATE * sizeof(int16_t)*2 / FPS) - SDL_GetAudioStreamQueued(audio_stream);
-    if (buffer_len <= 0) return;
-    void *buffer = malloc(buffer_len);
-    if (!buffer) return;
-    int samples = buffer_len / sizeof(int16_t) / 2;
-
-    for (int i = 0; i < samples; ++i) {
-        int square_wave = ((i / (square_wave_period/2)) % 2) ? tone_volume : -tone_volume;
-        ((int16_t*)buffer)[i*2] = square_wave;
-        ((int16_t*)buffer)[i*2+1] = square_wave;
-    }
-
-    SDL_PutAudioStreamData(audio_stream, buffer, buffer_len);
-
-    free(buffer);
-}
-
-int main(int argc, char *argv[]) {
-    BackBuffer back_buffer = {};
-    if (game_init(&back_buffer)) return 1;
+int main(void) {
+    BackBuffer back_buffer = {0};
+    AudioBuffer audio_buffer = {0};
+    if (game_init(&back_buffer, &audio_buffer)) return 1;
 
     is_running = true;
     while (is_running) {
@@ -300,9 +345,9 @@ int main(int argc, char *argv[]) {
         while (SDL_PollEvent(&event)) process_event(event, &back_buffer);
 
         game_tick();
-        audio();
 
         render_back_buffer(&back_buffer);
+        fill_audio_buffer(&audio_buffer);
 
         SDL_FRect frect = {0, 0, back_buffer.width, back_buffer.height};
         SDL_RenderTexture(renderer, back_buffer.texture, &frect, &frect);
@@ -310,6 +355,10 @@ int main(int argc, char *argv[]) {
     }
 
     free(back_buffer.memory);
+    free(audio_buffer.memory);
+
+    if (audio_stream) SDL_DestroyAudioStream(audio_stream);
+    if (gamepad) SDL_CloseGamepad(gamepad);
 
     SDL_DestroyTexture(back_buffer.texture);
     SDL_DestroyRenderer(renderer);
